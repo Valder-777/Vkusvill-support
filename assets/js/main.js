f9c7daa586245b4763bb9c6a69a9d0b5f7a11e3e
Valder-777 <v.i.pluzhnikov86@gmail.com>
2024-03-19 22:41:19 +0300

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,42 @@
 // Swiper Slider
-var swiper = new Swiper(".slider-content", {
-  slidesPerView: 4,
-  centeredSlides: false,
-  gragCursor: "true",
-  spaceBetween: 0,
-  pagination: {
-    el: ".swiper-pagination",
-    
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+let sliders = document.querySelectorAll(".slider");
+if (sliders.length > 0) {
+  var swiper = new Swiper(".slider", {
+    gragCursor: "true",
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 24,
+      },
+      576: {
+        slidesPerView: 1,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      991: {
+        slidesPerView: 2.5,
+        spaceBetween: 24,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+    }, 
+    pagination: {
+      el: ".swiper-pagination",
+      
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+  });
+}  
+
 
 // Accordion
 
